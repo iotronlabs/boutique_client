94fc8f8a40a602e5f39fb690f6aca3700b15f235
saumyagoyal1708 <1705071@kiit.ac.in>
2019-10-03 15:39:49 +0530

    product page mobile

--- pages/Package.vue
+++ pages/Package.vue
@@ -1,22 +1,22 @@
 <template>
 <v-container fluid>
-    <v-row align-center justify-center v-for = "product in products" :key = "product">
-        <v-col cols = "12" md = "1"></v-col>
-        <v-col cols = "12" xs = "12" sm = "12" md = "10">
+    <v-row align-center justify-center v-for = "product in products" :key = "product" class = "hidden-sm-and-down">
+        <v-col cols = "12" md = "1" lg = "1"></v-col>
+        <v-col cols = "12" xs = "12" sm = "12" md = "10" lg = "10">
             <v-card
             class="mx-auto"
-            height = "500"
+            height = "550"
             >
                 <v-row
                 class="py-4 pl-4"
                 >
-                    <v-col class="shrink" cols = "12" xs = "6" sm = "6" md = "6">
+                    <v-col class="shrink" cols = "12" xs = "6" sm = "6" md = "6" lg = "6">
                         <v-img
-                            height="450"
-                            src="dress.webp"
+                            height="500"
+                            :src="product.src"
                         ></v-img>
                     </v-col>
-                    <v-col cols = "12" xs = "6" sm = "6" md = "6">
+                    <v-col cols = "12" xs = "6" sm = "6" md = "6" lg = "6">
                        <p class = "font-weight-bold display-1"> {{product.title}}</p>
                        <p>{{product.subtitle}}</p>
                         <v-div>
@@ -24,27 +24,53 @@
                         </v-div>
                         <br>
                         <v-row>
-                        <v-col cols = "12" xs = "6" sm= "6" md = "3" class="font-weight-bold">Price : {{product.dprice}} 
+                        <v-col cols = "12" xs = "6" sm= "6" md = "3" lg= "3" class="font-weight-bold">Price : {{product.dprice}} 
 
                         </v-col>
-                         <v-col cols = "12" xs = "6" sm= "6" md = "3" > {{product.oprice}} 
+                         <v-col cols = "12" xs = "6" sm= "6" md = "3" lg = "3" > {{product.oprice}} 
 
                         </v-col>
                         
                         </v-row>
-                        <v-div>
-                            <p class="font-weight-bold">Colors : </p>
-                            <v-chip>green</v-chip>
-                            <v-chip>red</v-chip>
-                            <v-chip>yellow</v-chip>
-                        </v-div>
+                       <!-- <v-chip-group
+                        multiple
+                        column
+                        active-class="primary--text"
+                        >
+                            <v-chip v-for="color in colors" :key="color">
+                                {{ color }}
+                            </v-chip>
+                        </v-chip-group>-->
+
+                        <p class="font-weight-bold">Colors : </p>
+                       <!-- <v-div v-for = "color in colors" :key = "color">
+
+                                <v-chip>{{color.data}}</v-chip>&emsp;
+                            </v-div>-->
+                            <v-chip-group
+                            multiple
+                            column
+                            active-class="primary--text"
+                            >
+                                <v-chip v-for="color in colors" :key="color">
+                                    {{ color.data }}
+                                </v-chip>
+                            </v-chip-group>
                         
-                        <v-div>
-                            <p class="font-weight-bold">Size : </p>
-                            <v-chip>S</v-chip>
-                            <v-chip>M</v-chip>
-                            <v-chip>L</v-chip>
-                        </v-div>
+                         <p class="font-weight-bold">Size : </p>
+                       <!-- <v-div v-for = "size in sizes" :key = "size">
+                           <v-chip>{{size.data}}</v-chip>&emsp;
+                            
+                        </v-div>-->
+                        <v-chip-group
+                        multiple
+                        column
+                        active-class="primary--text"
+                        >
+                            <v-chip v-for="size in sizes" :key="size">
+                                {{ size.data }}
+                            </v-chip>
+                        </v-chip-group>
                         
                         <v-row class = "pa-8">
                         <v-btn>Add To Bag</v-btn><v-spacer></v-spacer>
@@ -67,13 +93,45 @@ export default {
         return{
             products:[
                 {
+                    src : "/dress.webp",
                     title : "Women Navy Blue  Dress",
                     subtitle : "Vero Moda",
                     dprice : "Rs 799",
                     oprice : "Rs 1000 (20% off)",
+                   // colors:['tyf' ,'dfghj' ,'dfghj']
                 },
+                
+                
+                
             ],
             rating : "4",
+
+            colors:[
+                {
+                    data:"blue",
+                },
+                {
+                    data:"green",
+                },
+                {
+                    data:"purple",
+                },
+            ],
+
+            sizes:[
+                {
+                    data :"S",
+                },
+                {
+                    data : "M",
+                },
+                {
+                    data : "L",
+                },
+                {
+                    data : "XL",
+                },
+            ]
         }
     }
 
---
+++ pages/Package_mobile.vue
@@ -0,0 +1,168 @@
+<template>
+<v-container fluid>
+    <v-row >
+        <v-col cols = "12" xs = "6" sm = "6"  v-for = "product in products" :key = "product" class = "hidden-md-and-up" >
+            <v-card
+             class="mx-auto"
+            :flat="false"
+            :loading="false"
+            :outlined="false"
+            :elevation="undefined"
+            :raised="false"
+            :width="undefined"
+            :max-height="550">
+            
+                <v-carousel :show-arrows="true"  hide-delimiters  height="250" style="width:100%">
+                    <v-carousel-item
+                    v-for="(image,i) in images"
+                    :key="i"
+                    :src="image.src"
+                    >
+                    </v-carousel-item>
+                </v-carousel>
+                
+        
+                <v-card-text>
+                    <v-row>
+                        <v-col cols = "12" xs = "6" sm = "6">
+                            <p class = "font-weight-bold title black--text"> {{product.title}}&emsp;-</p>
+                        </v-col>
+                        <v-col cols = "12" xs = "6" sm = "6" >
+                            <p class = "title"> {{product.subtitle}}</p>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols = "12" xs = "6" sm = "6">
+                            <p class = "font-weight-bold title black--text"> {{product.dprice}}&emsp;-</p>
+                        </v-col>
+                        <v-col cols = "12" xs = "6" sm = "6" >
+                            <p class = "title"> {{product.oprice}}</p>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols = "12" xs = "6" sm = "6" >
+                            <p class = "font-weight-bold title black--text">Colors : </p>
+                        </v-col>
+                        <v-col cols = "12" xs = "6" sm = "6">
+                            <v-chip-group
+                            multiple
+                            column
+                            active-class="primary--text"
+                            >
+                                <v-chip v-for="color in colors" :key="color">
+                                    {{ color.data }}
+                                </v-chip>
+                            </v-chip-group>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols = "12" xs = "6" sm = "6">
+                           <p class = "font-weight-bold title black--text">Size : </p>
+                        </v-col>
+                        <v-col cols = "12" xs = "6" sm = "6">
+                             <v-chip-group
+                            multiple
+                            column
+                            active-class="primary--text"
+                            >
+                                <v-chip v-for="size in sizes" :key="size">
+                                    {{ size.data }}
+                                </v-chip>
+                        </v-chip-group>
+                        </v-col>
+                    </v-row>
+                    
+                </v-card-text>
+        
+            </v-card>
+        </v-col>
+
+        
+    </v-row>
+    <v-app-bar
+    fixed
+      app
+      light
+      bottom
+      floating
+      class="hidden-md-and-up"
+      min-height="30px"
+      align-center justify-center>
+      <v-row class = "pr-8 pl-n8">
+           <v-col >
+          </v-col>
+          <v-col >
+              <v-btn>Add To Bag</v-btn>
+          </v-col>
+          <v-col >
+              
+          </v-col>
+           <v-col >
+               <v-btn> Wishlist</v-btn>
+          </v-col>
+      </v-row>
+    </v-app-bar>
+</v-container>
+    
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            media : 'true',
+            images:[
+                {
+                    src : "dress.webp"
+                },
+                 {
+                    src : "dress.webp"
+                },
+                
+            ],
+            products:[
+                {
+                    title : "Women Navy Blue  Dress",
+                    subtitle : "Vero Moda",
+                    dprice : "Rs 799",
+                    oprice : "Rs 1000 (20% off)",
+                   // colors:['tyf' ,'dfghj' ,'dfghj']
+                },
+                
+                
+            ],
+            colors:[
+                {
+                    data:"blue",
+                },
+                {
+                    data:"green",
+                },
+                {
+                    data:"purple",
+                },
+            ],
+
+            sizes:[
+                {
+                    data :"S",
+                },
+                {
+                    data : "M",
+                },
+                {
+                    data : "L",
+                },
+                {
+                    data : "XL",
+                },
+            ]
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
