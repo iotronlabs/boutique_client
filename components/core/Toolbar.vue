d77970ce21d89958d0a7983bd3cbaf20d7cc918d
iotronlab <iotronlabspvt@gmail.com>
2019-10-15 02:26:44 +0530

    Revert "UX"
    This reverts commit edf39aa55c872c19983fe163eba3071f8f63e0e2.

--- components/core/Toolbar.vue
+++ components/core/Toolbar.vue
@@ -1,45 +1,44 @@
 <template>
-  <div>
-    <v-app-bar app prominent>
-      <v-app-bar-nav-icon class="hidden-md-and-up" @click="toggleDrawer"></v-app-bar-nav-icon>
-      <!-- <v-toolbar-side-icon  /> -->
+<div>
+  	<v-app-bar app prominent>
+		<v-app-bar-nav-icon class="hidden-md-and-up mt-5" @click="toggleDrawer"></v-app-bar-nav-icon>
+    	<!-- <v-toolbar-side-icon  /> -->
+		
+		<v-container fluid px-0 py-0>
+			<v-row no-gutters>
+				<v-col class="d-flex justify-center">
+				<v-img
+					src="/butiq.png"
+					contain
+					height="90"
+					width="90"
+					max-width="90"
+					@click="$vuetify.goTo(0)"
+				/>
+				</v-col>
+				<v-col class="d-flex align-center">
+					<v-text-field
+						append-icon="mdi-magnify"
+						label="Customise your Style"
+						hide-details
+						outlined
+						rounded
+						class="hidden-sm-and-down"
+					/>
+				</v-col>
+				<v-col class="d-flex justify-center align-center">
 
-      <v-container fluid px-0 py-0 mt-n2>
-        <v-row no-gutters>
-          <v-col class="d-flex justify-center">
-            <v-img
-              src="/butiq.png"
-              contain
-              height="90"
-              width="90"
-              max-width="90"
-              @click="$vuetify.goTo(0)"
-            />
-          </v-col>
-          <v-col class="d-flex align-center">
-            <v-text-field
-              append-icon="mdi-magnify"
-              label="Customise your Style"
-              hide-details
-              outlined
-              rounded
-              class="hidden-sm-and-down"
-            />
-          </v-col>
-          <v-col class="d-flex justify-center align-center">
-            <span class="hidden-sm-and-down mr-2">
-              <Login />
-            </span>
-
-            <!-- <v-btn color="primary" dark class="hidden-sm-and-down">Login</v-btn> -->
-            <v-btn rounded outlined color="primary">
-              Cart
-              <v-icon right>mdi-cart</v-icon>
-            </v-btn>
-          </v-col>
-        </v-row>
-        <v-row class="d-flex justify-center mt-n2">
-          <!--<div class="hidden-sm-and-down navbar-menu" >
+			
+					<span class="hidden-sm-and-down mr-2"><Login /></span>
+			
+					<!-- <v-btn color="primary" dark class="hidden-sm-and-down">Login</v-btn> -->
+					<v-btn rounded outlined color="primary" >
+						Cart<v-icon right>mdi-cart</v-icon>
+					</v-btn>
+				</v-col>
+			</v-row>
+				<v-row class="d-flex justify-center mt-n2">
+			<!--<div class="hidden-sm-and-down navbar-menu" >
 				<v-btn
 					v-for="(item, i) in items" :key="i"
 					:to="item.to"
@@ -51,24 +50,28 @@
 				>
 					{{item.text}}
 				</v-btn>
-          </div>-->
-          <div class="hidden-sm-and-down">
-            <v-menu open-on-hover bottom offset-y v-for="(item, i) in items" :key="i">
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  :to="item.to"
-                  router
-                  exact
-                  flat
-                  v-text="item.text"
-                  v-on="on"
-                  rounded
-                  outlined
-                  color="primary"
-                  class="mx-1"
-                >{{item.text}}</v-btn>
-              </template>
-              <!-- <v-list>
+			</div>-->
+			<div class="hidden-sm-and-down" >
+				<v-menu open-on-hover bottom offset-y v-for="(item, i) in items" :key="i">
+					<template v-slot:activator="{ on }">
+					<v-btn
+						:to="item.to"
+						router
+						exact
+						flat
+						v-text="item.text" 	
+						v-on="on"
+						rounded
+						outlined
+						color="primary"
+						class="mx-1"
+					>
+						{{item.text}}
+						
+					</v-btn>
+					
+					</template>
+					<!-- <v-list>
 						<v-list-item
 							v-for="sb in item.sub_items"
 							:key="sb"
@@ -77,56 +80,73 @@
 							<v-list-item-title>{{ sb.title }}</v-list-item-title>
 							<v-list-item-title>{{ categories.data }}</v-list-item-title>
 						</v-list-item>
-              </v-list>-->
-              <v-list v-for="item in categories.data" :key="item">
-                <v-list-item v-if="item.name=='Men'">
-                  <v-list-item-title>{{ item}}</v-list-item-title>
-                </v-list-item>
-              </v-list>
-              <v-list v-for="item in categories.data" :key="item">
-                <v-list-item v-if="item.name=='Women'">
-                  <v-list-item-title>{{ item}}</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-          </div>
-
-          <v-col class="hidden-md-and-up py-0 mt-n3">
-            <v-text-field outlined rounded hide-details label="Customise your Style" />
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-app-bar>
-  </div>
+					</v-list> -->
+					<v-list v-for="item in categories.data" :key="item"	>
+						<v-list-item
+						v-if="item.name=='Men'"
+						>
+							
+							<v-list-item-title>{{ item}}</v-list-item-title>
+							
+						</v-list-item>
+					</v-list>
+					<v-list v-for="item in categories.data" :key="item"	>
+						<v-list-item
+						v-if="item.name=='Women'"
+						>
+							
+							<v-list-item-title>{{ item}}</v-list-item-title>
+							
+						</v-list-item>
+					</v-list>
+					
+				</v-menu>
+   			</div>
+			   
+			<v-col class="hidden-md-and-up py-0">
+				<v-text-field
+					outlined
+					rounded
+					hide-details
+					label="Customise your Style"
+				/>
+			</v-col>
+		</v-row>
+			
+		</v-container>
+  	</v-app-bar>
+	
+</div>
 </template>
 
 <script>
-import { mapState, mapGetters, mapMutations } from "vuex";
-import Login from "@/components/Login";
+import { mapState,mapGetters, mapMutations } from 'vuex'
+import Login from '@/components/Login'
 export default {
-  components: {
-    Login
-  },
-  data() {
-    return {
-      hover: false
-    };
-  },
-  computed: {
-    // ...mapGetters({
-    // 	categories: 'categories'
+	components: {
+		Login
+	},
+	data() {
+		return {
+			hover: false
+		}
+	},
+    computed: {
+	    // ...mapGetters({
+		// 	categories: 'categories'
+			
+		// }),
+		...mapState({
+			items: state => state.layout.items,
+			categories: state => state.layout.categories
+		})
+    },
 
-    // }),
-    ...mapState({
-      items: state => state.layout.items,
-      categories: state => state.layout.categories
-    })
-  },
-
-  methods: {
-    ...mapMutations("layout", ["toggleDrawer"])
-  }
-};
+    methods: {
+      ...mapMutations('layout',['toggleDrawer']),
+	}
+}
 </script>
 <style scoped>
+
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,97 +1,103 @@
 <template>
-  <v-app>
-    <Slider />
+<v-app class="content">
 
-    <v-divider class="hidden-md-and-up"></v-divider>
-    <br />
-    <div class="text-center display-1 font-weight-medium">Shop By Category</div>
+  <Slider/>
+  
+	 <v-divider class="hidden-md-and-up"></v-divider><br>
+	<div class="black--text text-center display-1 font-weight-medium">
+		Shop By Category
+	</div>
 
-    <br />
-    <br />
-    <br />
-    <v-layout row wrap>
+  <br><br><br>
+   <v-layout row wrap>
+     
       <v-flex xs12 sm12 md3 v-for="n in 4" :key="n">
         <v-container>
-          <Card />
+          <Card/>
         </v-container>
       </v-flex>
+     
     </v-layout>
-    <v-divider></v-divider>
+   <v-divider></v-divider>
 
-    <br />
-    <br />
-    <br />&nbsp;
-    <v-divider></v-divider>
-    <br />
-    <div class="black--text text-center display-1 font-weight-medium">New Arrivals</div>
+   <br><br><br>
+  &nbsp; <v-divider></v-divider><br>
+  <div class="black--text text-center display-1 font-weight-medium">
+     New Arrivals
+  </div>
 
-    <br />
-    <br />
-    <br />
-    <v-layout row wrap>
+  <br><br><br>
+   <v-layout row wrap>
+     
       <v-flex xs12 sm12 md3 v-for="n in 4" :key="n">
         <v-container>
-          <Card />
+          <Card/>
         </v-container>
       </v-flex>
+     
     </v-layout>
-    <v-divider></v-divider>
+   <v-divider></v-divider>
 
-    <br />
-    <br />
-    <br />&nbsp;
-    <v-divider></v-divider>
-    <br />
-    <div class="black--text text-center display-1 font-weight-medium">Top Products</div>
+     <br><br><br>
+  &nbsp; <v-divider></v-divider><br>
+  <div class="black--text text-center display-1 font-weight-medium">
+     Top Products
+  </div>
 
-    <br />
-    <br />
-    <br />
-    <v-layout row wrap>
+  <br><br><br>
+   <v-layout row wrap>
+     
       <v-flex xs12 sm12 md3 v-for="n in 4" :key="n">
         <v-container>
-          <Card />
+          <Card/>
         </v-container>
       </v-flex>
+     
     </v-layout>
-    <v-divider></v-divider>
-    <br />
-    <br />
-    <br />&nbsp;
-    <v-divider></v-divider>
-    <br />
-    <div class="black--text text-center display-1 font-weight-medium">Best offers</div>
+   <v-divider></v-divider>
+       <br><br><br>
+  &nbsp; <v-divider></v-divider><br>
+  <div class="black--text text-center display-1 font-weight-medium">
+    Best offers
+  </div>
 
-    <br />
-    <br />
-    <br />
-    <v-layout row wrap>
+  <br><br><br>
+   <v-layout row wrap>
+     
       <v-flex xs12 sm12 md3 v-for="n in 4" :key="n">
         <v-container>
-          <Card />
+          <Card/>
         </v-container>
       </v-flex>
+     
     </v-layout>
-    <v-divider></v-divider>
-    <pre></pre>
-  </v-app>
+   <v-divider></v-divider>
+   <pre>
+
+
+
+   </pre>
+ </v-app>
 </template>
 
 
 <script>
-import Card from "@/components/Card";
-import Slider from "@/components/Slider";
+import Card from '@/components/Card'
+import Slider from '@/components/Slider'
 export default {
-  components: {
-    Card,
-    Slider
+  components:{
+    Card,Slider
   },
-  data() {
-    return {};
-  }
-};
+  data(){
+    return{
+     
+    }
+  },
+
+}
 </script>
 <style scoped>
+
 </style>
 
 
