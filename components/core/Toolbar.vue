001be97e4ba823a8c04c810941507f1f1e788828
Abhirup Datta <dattaabhirup712@gmail.com>
2019-10-19 00:22:51 +0530

    navbar update

--- components/core/Toolbar.vue
+++ components/core/Toolbar.vue
@@ -16,6 +16,65 @@
               @click="$vuetify.goTo(0)"
             />
           </v-col>
+
+          <!-- nav categories -->
+          <v-col class="d-flex align-center hidden-sm-and-down" >
+            <v-col v-for="category in categories.data"
+              :key="category">
+              <v-menu
+                open-on-hover
+                :close-on-content-click="false"
+                class="hidden-sm-and-down"
+                bottom
+                offset-y="5"
+              >
+              
+                <template v-slot:activator="{ on }"
+                >
+                  <v-btn text v-on="on">
+                    {{category.name}} 
+                  </v-btn>
+                </template>
+                
+                <v-card width="600px" height="300px">
+
+                  <v-card-title class="title">
+                    {{category.name}}
+                  </v-card-title>
+
+                  <v-row class="mx-5">
+                    <v-col cols="4" v-for="children in category.children"
+                    :key="children">
+                    <!-- <v-hover v-slot:default="{ hover }" value="true"> -->
+                      <v-row class="my-3">
+                        
+                      <v-list-item-title class="category-child-men">{{children.name}}</v-list-item-title>
+                      
+                      
+                      </v-row>
+                  
+                      <v-row v-for="subchildren in children.children"
+                        :key="subchildren">
+                        <v-list-item-subtitle class="category-sub-child">{{subchildren.name}}</v-list-item-subtitle>
+                      </v-row>
+                      
+                    </v-col>
+                    
+                  </v-row>
+
+
+                  
+                </v-card>
+
+              </v-menu>
+
+
+
+              
+            </v-col>
+          </v-col>
+          <v-col cols="1">
+          </v-col>
           <v-col class="d-flex align-center">
             <v-text-field
               append-icon="mdi-magnify"
@@ -36,23 +95,12 @@
               Cart
               <v-icon right>mdi-cart</v-icon>
             </v-btn>
+            </v-col>
           </v-col>
         </v-row>
         <v-row no-gutters class="d-flex justify-center mt-n2">
-          <!--<div class="hidden-sm-and-down navbar-menu" >
-				<v-btn
-					v-for="(item, i) in items" :key="i"
-					:to="item.to"
-					router
-					exact
-					flat
-					v-text="item.text" 
-					class="navbar-menu-items"	
-				>
-					{{item.text}}
-				</v-btn>
-          </div>-->
-          <div class="hidden-sm-and-down">
+        
+          <!-- <div class="hidden-sm-and-down">
             <v-menu open-on-hover bottom offset-y v-for="(item, i) in items" :key="i">
               <template v-slot:activator="{ on }">
                 <v-btn
@@ -68,16 +116,7 @@
                   class="mx-1"
                 >{{item.text}}</v-btn>
               </template>
-              <!-- <v-list>
-						<v-list-item
-							v-for="sb in item.sub_items"
-							:key="sb"
-							
-						>
-							<v-list-item-title>{{ sb.title }}</v-list-item-title>
-							<v-list-item-title>{{ categories.data }}</v-list-item-title>
-						</v-list-item>
-              </v-list>-->
+              
               <v-list v-for="item in categories.data" :key="item">
                 <v-list-item v-if="item.name=='Men'">
                   <v-list-item-title>{{ item}}</v-list-item-title>
@@ -89,7 +128,7 @@
                 </v-list-item>
               </v-list>
             </v-menu>
-          </div>
+          </div> -->
 
           <v-col class="hidden-md-and-up py-0 mt-n3">
             <v-text-field outlined rounded hide-details label="Customise your Style" />
@@ -128,5 +167,24 @@ export default {
   }
 };
 </script>
+
+
 <style scoped>
+  .category-sub-child:hover{
+    margin-left: 2px;
+    font-weight: bold;
+  }
+  .category-child-men {
+    color: blue;
+    font-weight: bold;
+  }
+  .category-child-women {
+    color: red;
+    font-weight: bold;
+  }
+  .category-child-art {
+    color: green;
+    font-weight: bold;
+  }
+  
 </style>
